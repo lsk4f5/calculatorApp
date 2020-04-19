ebdd6843edc5076de59946d4eebfde47bdef03da
miseri <miseri.osk12@gmail.com>
2020-04-20 02:36:00 +0900

    追加のコミット

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = caluculator.dequeueReusableCell(withReuseIdentifier: "cellID", for: indexPath)
+        
+        return cell
+    }
+    
 
+    @IBOutlet weak var caluculator: UICollectionView!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var calculatorHeight: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        caluculator.delegate = self
+        caluculator.dataSource = self
+        caluculator.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellID")
     }
 
 
